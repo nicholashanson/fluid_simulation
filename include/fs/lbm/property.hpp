cf60c1d3261bea55a81600b2b1f687f229b05d92
Nick <nicholashanson27@outlook.com>
2025-02-22 10:11:51 +0800

    added curl

--- include/fs/lbm/property.hpp
+++ include/fs/lbm/property.hpp
@@ -63,6 +63,62 @@ namespace fs {
             return u_y;
         }
 
+        template<typename DataStorage, typename View>
+        sim::grid<std::vector<double>, property_view>
+        calculate_property_v( const sim::grid<DataStorage, View>& gd, const std::vector<double>& property_states, 
+                              std::function<double( std::array<double,9>& )> calculate_property ) {
+
+            sim::grid<std::vector<double>, property_view> property_grid( property_states );
+            
+            const size_t ydim = gd.get_dim( 0 );
+            const size_t xdim = gd.get_dim( 1 );
+
+            for ( size_t y = 0; y < ydim; ++y ) {
+                for ( size_t x = 0; x < xdim; ++x ) {
+
+                    std::array<double, 9> cell_state = gd.get_cell_state_array( y, x );
+
+                    double property = calculate_property( cell_state );
+
+                    property_grid.set_cell_state( property, y, x );
+                }
+            }
+
+            return property_grid;
+        }
+
+        template<typename DataStorage, typename View>
+        std::tuple<double, sim::grid<std::vector<double>, property_view>>
+        calculate_curl_with_max( const sim::grid<DataStorage, View>& gd, const std::vector<double>& property_states ) {
+
+            sim::grid<std::vector<double>, property_view> u_x = calculate_property_v( gd, property_states, calculate_u_x );
+            sim::grid<std::vector<double>, property_view> u_y = calculate_property_v( gd, property_states, calculate_u_y );
+
+            sim::grid<std::vector<double>, property_view> curl( property_states );
+
+            const size_t ydim = gd.get_dim( 0 );
+            const size_t xdim = gd.get_dim( 1 );
+
+            double max_curl{};
+
+            std::cout << "about to enter loop" << std::endl;
+
+            for ( size_t y = 1; y < ydim - 1; ++y ) {
+                for ( size_t x = 1; x < xdim - 1; ++x ) {
+
+                    double curl_ = u_x.get_cell_state( y, x + 1 ) - u_y.get_cell_state( y, x - 1 )
+                                   - u_x.get_cell_state( y + 1, x ) + u_x.get_cell_state( y - 1, x );
+
+                    if ( curl_ > max_curl )
+                        max_curl = curl_;
+
+                    curl.set_cell_state( curl_, y, x );
+                }
+            }
+
+            return std::make_tuple( max_curl, std::move( curl ) );
+        }
+
         // takes a D2Q9 grid and outputs a grid of a certain property defined by calculate_property
         // e.g. fs::lbm::calculate_property_v_with_max( ..., fs::lbm::calculate_rho ) will return a 
         // a gird object of densities and the max density as a tuple
