3868eeb55b9a476deb17f3a07440a89d1b67f422
Nick <nicholashanson27@outlook.com>
2025-02-22 16:23:31 +0800

    updated settings

--- include/settings.hpp
+++ include/settings.hpp
@@ -1,15 +1,22 @@
 #ifndef SETTINGS_HPP
 #define SETTINGS_HPP
 
+#include <cstddef>
+
 namespace fs {
 
     namespace settings {
 
-        constexpr size_t grid_width = 100;
-        constexpr size_t grid_height = 250;
+        constexpr size_t xdim = 32 * 20;
+        constexpr size_t ydim = 32 * 8;
 
-        constexpr int window_width = 1000;
-        constexpr int window_height = 400;
+        constexpr size_t pixels_per_cell = 2;
+
+        constexpr int window_width = pixels_per_cell * xdim;
+        constexpr int window_height = pixels_per_cell * ydim;
+
+        constexpr size_t vertex_n = 6;
+        constexpr size_t vertex_data_n = 5;
 
     }
 
