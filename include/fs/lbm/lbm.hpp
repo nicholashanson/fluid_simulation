a7f22bd7d18e1b703950709265a60aa2198d6d32
Nick <nicholashanson27@outlook.com>
2025-02-23 20:35:11 +0800

    removed includes

--- include/fs/lbm/lbm.hpp
+++ include/fs/lbm/lbm.hpp
@@ -8,16 +8,8 @@
 #include <fs/lbm/common.hpp>
 
 #if !defined(DPCPP_COMPILER)
-#include <fs/lbm/grid_to_vertex_data_interface.hpp>
 #include <fs/lbm/collide_and_stream_dpcxx_interface.hpp>
-#include <fs/lbm/collision_step_tbb_thread_pool.hpp>
-#include <fs/lbm/collision_step_serial.hpp>
-//#include <fs/lbm/streaming_step.hpp>
-//#include <fs/lbm/set_inlet.hpp>
 #include <fs/lbm/initialize_grid.hpp>
-//#include <fs/lbm/handle_boundaries.hpp>
-#include <fs/lbm/collision_step.hpp>
-//#include <fs/lbm/velocity.hpp>
 #include <fs/lbm/property.hpp>
 #endif
 
