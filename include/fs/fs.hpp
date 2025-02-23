ad3b7caeafd0555ea84ca1f8af35b369dcf6ad13
Nick <nicholashanson27@outlook.com>
2025-02-23 17:43:40 +0800

    added fs

---
+++ include/fs/fs.hpp
@@ -0,0 +1,13 @@
+#ifndef FS_HPP
+#define FS_HPP
+
+#include <grid.hpp>
+
+#include <fs/lbm/lbm.hpp>
+
+#if !defined( DPCPP_COMPILER )
+#include <shader.hpp>
+#include <grid_renderer.hpp>
+#endif
+
+#endif
