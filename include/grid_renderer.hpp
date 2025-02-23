52afd2287dde4b6b6d392aef009d0eb0938115fa
Nick <nicholashanson27@outlook.com>
2025-02-24 07:05:16 +0800

    updated renderer

--- include/grid_renderer.hpp
+++ include/grid_renderer.hpp
@@ -3,21 +3,29 @@
 
 #include <vector>
 #include <grid.hpp>
+#include <functional>
+#include <array>
 
-namespace fs {
-
-    unsigned int setup_shaders( const char * vertex_path, const char * fragment_path );
-
-    template<typename Array, typename MDSpan>
-    std::vector<float> grid_to_vertex_data( const grid<Array, MDSpan>& gd );
-
-    template<typename Array, typename MDSpan>
-    std::vector<float> lb_to_vertex_data( const grid<Array, MDSpan>& gd, std::vector<double>& vec_mag_states );
+namespace app {
 
     void setup_grid_buffers( const std::vector<float>& vertices, unsigned int& VAO, unsigned int& VBO );
 
     void render_grid( unsigned int VAO, size_t vertex_count );
 
+    template<size_t ydim, size_t xdim>
+    std::vector<float> obstacle_to_vertex_data( const std::array<bool, xdim*ydim>& barrier );
+
+    template<size_t ydim, size_t xdim>
+    std::vector<float> js( const std::array<double, xdim*ydim>& u_x );
+
+    template<size_t ydim, size_t xdim>
+    std::vector<float> js_tbb( const std::array<double, xdim*ydim>& u_x );
+
+    /*
+    template<typename Array, typename MDSpan>
+    std::vector<float> property_to_vertex_data( const sim::grid<Array, MDSpan>& gd, std::vector<double>& property_states,
+                                                std::function<double(std::array<double, 9> calculate_property )> );
+    */
 }
 
 #include <grid_renderer.inl> // include template implementations
