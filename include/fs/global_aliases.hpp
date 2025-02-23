b3add0f2c2a139bf441fa063d4185f41f436b354
Nick <nicholashanson27@outlook.com>
2025-02-23 18:43:56 +0800

    added ga

---
+++ include/fs/global_aliases.hpp
@@ -0,0 +1,24 @@
+#ifndef GLOBAL_ALIASES_HPP
+#define GLOBAL_ALIASES_HPP
+
+#ifdef DPCPP_COMPILER
+#include <sycl/sycl.hpp>
+#endif
+
+#include <mdspan-stable/include/mdspan/mdspan.hpp>
+#include <settings.hpp>
+
+namespace fs {
+
+    namespace lbm {
+
+        using D2Q9_view = Kokkos::mdspan<double, Kokkos::extents<size_t, settings::ydim, settings::xdim, 9>>;
+
+    } // lbm
+
+    using property_view = Kokkos::mdspan<double, Kokkos::extents<size_t, settings::ydim, settings::xdim>>;
+    using vertex_view = Kokkos::mdspan<float, Kokkos::extents<size_t, settings::ydim, settings::xdim, 5*6>>;
+
+} // fs
+
+#endif
