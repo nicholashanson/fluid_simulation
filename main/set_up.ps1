2ab80a7af76c8f00f317a2bc690c20e8747a9248
Nick <nicholashanson27@outlook.com>
2025-02-28 00:04:32 +0800

    msys tbb

--- main/set_up.ps1
+++ main/set_up.ps1
@@ -563,9 +563,7 @@ function Compile-Code {
         ($files | ForEach-Object { $_ }) + " " +
         ($includes | ForEach-Object { "-I" + (Join-Path (Get-Location) $_) }) + " " +
         "-IC:\tools\opencv\build\include " +
-        "-I`"C:\Program Files (x86)\Intel\oneAPI\tbb\2022.0\include`" " +
-        "-L`"C:\Program Files (x86)\Intel\oneAPI\tbb\2022.0\lib`" " +
-        "-lopengl32 -lglfw3 -lgdi32 -ltbb12 -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs"
+        "-lopengl32 -lglfw3 -lgdi32 -lopencv_core -lopencv_imgproc -lopencv_highgui -lopencv_imgcodecs"
 
     # Execute the build
     Invoke-Expression $compileCommand
