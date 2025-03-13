0917f5a46d9e1aad398f28d8e8e18a0334614b6e
Nick <nicholashanson27@outlook.com>
2025-03-13 19:05:20 +0000

    removed declarations

--- include/grid_renderer.hpp
+++ include/grid_renderer.hpp
@@ -11,21 +11,6 @@ namespace app {
     void setup_grid_buffers( const std::vector<float>& vertices, unsigned int& VAO, unsigned int& VBO );
 
     void render_grid( unsigned int VAO, size_t vertex_count );
-
-    template<size_t ydim, size_t xdim>
-    std::vector<float> obstacle_to_vertex_data( const std::array<bool, xdim*ydim>& barrier );
-
-    template<size_t ydim, size_t xdim>
-    std::vector<float> js( const std::array<double, xdim*ydim>& u_x );
-
-    template<size_t ydim, size_t xdim>
-    std::vector<float> js_tbb( const std::array<double, xdim*ydim>& u_x );
-
-    /*
-    template<typename Array, typename MDSpan>
-    std::vector<float> property_to_vertex_data( const sim::grid<Array, MDSpan>& gd, std::vector<double>& property_states,
-                                                std::function<double(std::array<double, 9> calculate_property )> );
-    */
 }
 
 #include <grid_renderer.inl> // include template implementations
