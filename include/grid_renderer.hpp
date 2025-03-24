7a30b3b81ccd26259c8415a0dabc6d91ac97c7c4
Nick <nicholashanson27@outlook.com>
2025-03-24 08:31:51 +0000

    3D

--- include/grid_renderer.hpp
+++ include/grid_renderer.hpp
@@ -10,7 +10,21 @@ namespace app {
 
     void setup_grid_buffers( const std::vector<float>& vertices, unsigned int& VAO, unsigned int& VBO );
 
+    void init_grid_buffers( const std::vector<float>& vertices, unsigned int& VAO, unsigned int& VBO );
+
+    void init_grid_buffers_3D( const std::vector<float>& vertices, unsigned int& VAO, unsigned int& VBO );
+
+    void refresh_grid_buffers( const std::vector<float>& vertices, unsigned int VBO );
+
     void render_grid( unsigned int VAO, size_t vertex_count );
+
+    void render_wiremesh( unsigned int VAO, size_t vertex_count );
+        
+    void generate_wiremesh_vertices( std::vector<float>& vertices );
+
+    std::vector<float> axes();
+
+    std::vector<float> boundaries();
 }
 
 #include <grid_renderer.inl> // include template implementations
