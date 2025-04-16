1c10c0416a594c7108dde938c93e5386d41904de
Nick <nicholashanson27@outlook.com>
2025-04-16 22:35:34 +0000

    representative points

--- include/fs/fvm/triangulation.hpp
+++ include/fs/fvm/triangulation.hpp
@@ -4,12 +4,49 @@
 #include <cmath>
 
 #include <set>
+#include <map>
 #include <tuple>
 #include <vector>
 
 namespace fs {
 
     namespace fvm {
+
+        template<typename I,typename T>
+        struct representative_coords {
+            T x;
+            T y;
+            I n;
+
+            representative_coords( T x, T y, T n ) : x( x ), y( y ), n( n ) {}
+        };
+
+        template<typename I,typename T>
+        using BPL = std::map<I, representative_coords<I,T>>;
+
+        template<typename I,typename T>
+        class triangulation;
+        
+        template<typename I,typename T>
+        void reset_representative_coordinates( triangulation<I,T>& tri ) {
+            for ( auto& representative_point : tri.get_representative_points() ) {
+                representative_point.x = ( T )0;
+                rerpesentative_point.y = ( T )0;
+                representative_point.n = ( I )0; 
+            } 
+        }
+
+        template<typename I,typename T>
+        class triangulation {
+        public:
+
+            // Rule-of-Zero
+            BPL<I,T> get_representative_points() {
+                return representative_points;
+            }
+        private:
+            BPL<I,T> representative_points;
+        };
 
         using triangle = std::tuple<int,int,int>;
 
@@ -23,7 +60,7 @@ namespace fs {
 
         inline size_t sub_2_ind( const size_t x, const size_t y, const size_t xdim ) {
 
-            return std::fma( y - 1, xdim, x );
+            return std::fma( y, xdim, x );
         }
 
         inline triangle_set get_lattice_triangles( const size_t ydim, const size_t xdim ) {
@@ -33,15 +70,15 @@ namespace fs {
             for ( size_t y = 1; y < ydim; ++y ) {
                 for ( size_t x = 1; x < xdim; ++x ) {
 
-                    int u = sub_2_ind( x, y, xdim );
-                    int v = sub_2_ind( x + 1, y, xdim ); 
-                    int w = sub_2_ind( x, y + 1, xdim );
+                    int u = sub_2_ind( x, y - 1, xdim );
+                    int v = sub_2_ind( x + 1, y - 1, xdim ); 
+                    int w = sub_2_ind( x, y, xdim );
 
                     triangles.insert( std::make_tuple( u, v, w ) );
 
-                    u = sub_2_ind( x, y + 1, xdim );
-                    v = sub_2_ind( x + 1, y, xdim ); 
-                    w = sub_2_ind( x + 1, y + 1, xdim );
+                    u = sub_2_ind( x, y, xdim );
+                    v = sub_2_ind( x + 1, y - 1, xdim ); 
+                    w = sub_2_ind( x + 1, y, xdim );
 
                     triangles.insert( std::make_tuple( u, v, w ) );
                 }
@@ -74,6 +111,25 @@ namespace fs {
             }
 
             return points;
+        }
+
+        inline std::vector<int> get_lattice_boundary( const size_t xdim, const size_t ydim ) {
+   
+            std::vector<int> boundary( 2 * xdim + 2 * ydim - 4 );
+    
+            for ( size_t x = 0; x < xdim; ++x ) {
+
+                boundary[ x ] = sub_2_ind( x, 0, xdim );
+                boundary[ ( xdim - 1 ) + x ] = sub_2_ind( x, ydim - 1, xdim );
+            }
+
+            for ( size_t y = 1; y < ydim - 1; ++y ) {
+
+                boundary[ 2 * ( xdim - 1 ) + y - 1 ] = sub_2_ind( 0, y, xdim );
+                boundary[ 2 * ( xdim - 1 ) + ( ydim - 2 ) + y ] = sub_2_ind( xdim, y, xdim );
+            }
+
+            return boundary;
         }
 
     } // namespace fvm
