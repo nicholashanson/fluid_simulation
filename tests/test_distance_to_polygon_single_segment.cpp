e4ebe15f0a6af385255f51d20c537aad10a1d7d5
Nick <nicholashanson27@outlook.com>
2025-04-19 02:20:29 +0000

    test constants

--- tests/test_distance_to_polygon_single_segment.cpp
+++ tests/test_distance_to_polygon_single_segment.cpp
@@ -2,82 +2,59 @@
 
 #include <fs/fvm/triangulation.hpp>
 
-TEST( DistanceToPolygonSingleSegment, CenterPointBottomEdge ) {
+#include "test_constants.hpp"
 
-    std::vector<std::pair<double, double>> points = { {  0.0,  0.0 }, 
-                                                      { 10.0,  0.0 }, 
-                                                      { 10.0, 10.0 }, 
-                                                      {  0.0, 10.0 } };
+TEST( DistanceToPolygonSingleSegment, CenterPointBottomEdge ) {
 
     std::vector<size_t> boundary_nodes = { 0, 1 };
 
     std::pair<double, double> p = { 5.0, 5.0 };
 
-    double dist = fs::fvm::distance_to_polygon_single_segment( p, points, boundary_nodes );
+    double dist = fs::fvm::distance_to_polygon_single_segment( p, test::square_points, boundary_nodes );
 
     EXPECT_DOUBLE_EQ( dist, -5.0 );
 }
 
 TEST( DistanceToPolygonSingleSegment, CenterPointLeftEdge ) {
 
-    std::vector<std::pair<double, double>> points = { {  0.0,  0.0 }, 
-                                                      { 10.0,  0.0 }, 
-                                                      { 10.0, 10.0 }, 
-                                                      {  0.0, 10.0 } };
-
     std::vector<size_t> boundary_nodes = { 0, 3 };
 
     std::pair<double, double> p = { 5.0, 5.0 };
 
-    double dist = fs::fvm::distance_to_polygon_single_segment( p, points, boundary_nodes );
+    double dist = fs::fvm::distance_to_polygon_single_segment( p, test::square_points, boundary_nodes );
 
     EXPECT_DOUBLE_EQ( dist, -5.0 );  
 }
 
 TEST( DistanceToPolygonSingleSegment, CenterPointRightEdge ) {
 
-    std::vector<std::pair<double, double>> points = { {  0.0,  0.0 }, 
-                                                      { 10.0,  0.0 }, 
-                                                      { 10.0, 10.0 }, 
-                                                      {  0.0, 10.0 } };
-
     std::vector<size_t> boundary_nodes = { 1, 2 };
 
     std::pair<double, double> p = { 5.0, 5.0 };
 
-    double dist = fs::fvm::distance_to_polygon_single_segment( p, points, boundary_nodes );
+    double dist = fs::fvm::distance_to_polygon_single_segment( p, test::square_points, boundary_nodes );
 
     EXPECT_DOUBLE_EQ( dist, 5.0 ); 
 }
 
 TEST( DistanceToPolygonSingleSegment, CenterPointUpperEdge ) {
 
-    std::vector<std::pair<double, double>> points = { {  0.0,  0.0 }, 
-                                                      { 10.0,  0.0 }, 
-                                                      { 10.0, 10.0 }, 
-                                                      {  0.0, 10.0 } };
-
     std::vector<size_t> boundary_nodes = { 3, 2 };
 
     std::pair<double, double> p = { 5.0, 5.0 };
 
-    double dist = fs::fvm::distance_to_polygon_single_segment( p, points, boundary_nodes );
+    double dist = fs::fvm::distance_to_polygon_single_segment( p, test::square_points, boundary_nodes );
 
     EXPECT_DOUBLE_EQ( dist, -5.0 ); 
 }
 
 TEST( DistanceToPolygonSingleSegment, PointOnBottomEdge ) {
 
-    std::vector<std::pair<double, double>> points = { {  0.0,  0.0 }, 
-                                                      { 10.0,  0.0 }, 
-                                                      { 10.0, 10.0 }, 
-                                                      {  0.0, 10.0 } };
-
     std::vector<size_t> boundary_nodes = { 0, 1 };
 
     std::pair<double, double> p = { 5.0, 0.0 };
 
-    double dist = fs::fvm::distance_to_polygon_single_segment( p, points, boundary_nodes );
+    double dist = fs::fvm::distance_to_polygon_single_segment( p, test::square_points, boundary_nodes );
 
     EXPECT_DOUBLE_EQ( dist, 0.0 );  
 }
