97e6e42cee5af04a96175ad05f176d5b2709ec75
Nick <nicholashanson27@outlook.com>
2025-04-26 07:08:15 +0000

    adaptive predicates

--- tests/test_triangle_area.cpp
+++ tests/test_triangle_area.cpp
@@ -254,8 +254,22 @@ TEST( GeometryTests, Orient ) {
     ASSERT_EQ( expected_degenerate, fs::fvm::orient::DEGENERATE );  
 }
 
+/*
+    TEST: ConstructPositivelyOrientedTriangle
+
+    "points" is a pair of vectors:
+    - the first vector is x
+    - the second vector is y    
+
+*/
 TEST( GeometryTests, ConstructPositivelyOrientedTriangle ) {
 
+    /*
+        ( p, q, r ) is a right-angle triangle
+        p: x = 0.0, y = 0.0
+        q: x = 1.0, y = 0.0
+        q: x = 0.0, y = 1.0
+    */
     std::pair<double,double> p( 0.0, 0.0 );
     std::pair<double,double> q( 1.0, 0.0 );
     std::pair<double,double> r( 0.0, 1.0 );
@@ -264,8 +278,18 @@ TEST( GeometryTests, ConstructPositivelyOrientedTriangle ) {
         { p.first, r.first, q.first }, { p.second, r.second, q.second }
     );
 
+    /*
+        0, 1, 2 are the indices of p, q, r in "points"
+    */
     auto tri = fs::fvm::construct_positively_oriented_triangle<double>( points, 0, 1, 2 );
 
+    /*
+        v, u, w are the indices of the positively-oriented triangle, which should be:
+
+            0, 1, 2
+
+        because p, q, r are already positively-oriented
+    */
     auto [ v, u, w ] = tri.value();
 
     std::tie( p, q, r ) = fs::fvm::get_triangle_points( points, v, u, w );
