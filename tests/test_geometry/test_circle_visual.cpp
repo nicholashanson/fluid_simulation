523912520b38ad4050e6e0ce383ff9c6118020ce
nicholashanson <nicholashanson27@outlook.com>
2025-06-20 19:12:28 +0100

    increased number of vertices

--- tests/test_geometry/test_circle_visual.cpp
+++ tests/test_geometry/test_circle_visual.cpp
@@ -27,45 +27,44 @@ TEST( VisualGeometryTests, InCircle ) {
 
     auto triangle_center = fs::fvm::get_triangle_circumcenter( p, q, r );
     glm::vec2 circle_center( triangle_center.first, triangle_center.second );
-    auto circle_vertices = app::generate_circle_vertices( circle_center, 1.0 );
+    auto circle_vertices = app::generate_circle_vertices( circle_center, 1.0, 1000 );
 
     GLuint VAO, VBO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
+    glGenVertexArrays( 1, &VAO );
+    glGenBuffers( 1, &VBO );
 
     unsigned int shader_program = app::setup_openGL_for_test();
-    app::test_projection(shader_program);
-    app::identity_view(shader_program);
-    app::identity_model(shader_program);
+    app::test_projection( shader_program );
+    app::identity_view( shader_program );
+    app::identity_model( shader_program );
 
-    glBindVertexArray(VAO);
+    glBindVertexArray( VAO );
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, circle_vertices.size() * sizeof(glm::vec2), circle_vertices.data(), GL_STATIC_DRAW);
+    glBindBuffer( GL_ARRAY_BUFFER, VBO );
+    glBufferData( GL_ARRAY_BUFFER, circle_vertices.size() * sizeof( glm::vec2 ), circle_vertices.data(), GL_STATIC_DRAW );
 
-    // Setup vertex attribute for position at location 0 (vec2)
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(glm::vec2), (void*)0);
-    glEnableVertexAttribArray(0);
+    glVertexAttribPointer( 0, 2, GL_FLOAT, GL_FALSE, sizeof(glm::vec2), ( void* )0 );
+    glEnableVertexAttribArray( 0 );
 
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
-    glBindVertexArray(0);
+    glBindBuffer( GL_ARRAY_BUFFER, 0 );
+    glBindVertexArray( 0 );
 
     // Your drawing loop
     double start_time = glfwGetTime();
-    while (!glfwWindowShouldClose(window) && glfwGetTime() - start_time < 3.0) {
+    while ( !glfwWindowShouldClose(window) && glfwGetTime() - start_time < 3.0 ) {
 
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_LINE_LOOP, 0, circle_vertices.size());
-        glBindVertexArray(0);
-        glfwSwapBuffers(window);
+        glClearColor( 0.2f, 0.3f, 0.3f, 1.0f );
+        glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+        glBindVertexArray( VAO );
+        glDrawArrays( GL_LINE_LOOP, 0, circle_vertices.size() );
+        glBindVertexArray( 0 );
+        glfwSwapBuffers( window );
         glfwPollEvents();
     }
 
-    glDeleteBuffers(1, &VBO);
-    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers( 1, &VBO );
+    glDeleteVertexArrays( 1, &VAO );
 
-    glfwDestroyWindow(window);
+    glfwDestroyWindow( window );
     glfwTerminate();
 }
