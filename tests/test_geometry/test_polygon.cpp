6cab5361361a99c5c57b44e32b58fb7ebc797809
Nick <nicholashanson27@outlook.com>
2025-06-24 16:15:52 +0000

    center-right

--- tests/test_geometry/test_polygon.cpp
+++ tests/test_geometry/test_polygon.cpp
@@ -6,6 +6,7 @@
 
 #include <geometry/polygon.hpp>
 
+// center point above
 TEST( GeometryTests, DistanceToSingleSegment1 ) {
 
     std::vector<size_t> boundary_nodes = { 0, 1 };
@@ -13,4 +14,14 @@ TEST( GeometryTests, DistanceToSingleSegment1 ) {
     double dist = geometry::distance_to_polygon_single_segment( p, test::square_points, boundary_nodes );
 
     EXPECT_DOUBLE_EQ( dist, -5.0 );
+}
+
+// center point right
+TEST( GeometryTests, DistanceToSingleSegment2 ) {
+
+    std::vector<size_t> boundary_nodes = { 0, 3 };
+    std::pair<double, double> p = { 5.0, 5.0 };
+    double dist = geometry::distance_to_polygon_single_segment( p, test::square_points, boundary_nodes );
+
+    EXPECT_DOUBLE_EQ( dist, -5.0 );  
 }
