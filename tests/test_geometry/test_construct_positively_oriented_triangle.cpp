b80319c7636531f945de39be11dad65063937046
Nick <nicholashanson27@outlook.com>
2025-09-20 05:24:23 +0000

    Refactor triangle construction test: Improve clarity and streamline code
    - Removed unnecessary comments and refactored for clearer understanding.
    - Reorganized test case to improve readability and structure.
    - Simplified the initialization of the points vector with inline
      comments.
    - Replaced ASSERT statements with ASSERT_TRUE for better error
      reporting.

--- tests/test_geometry/test_construct_positively_oriented_triangle.cpp
+++ tests/test_geometry/test_construct_positively_oriented_triangle.cpp
@@ -2,41 +2,28 @@
 
 #include <geometry/triangle.hpp>
 
-/*
-    TEST: ConstructPositivelyOrientedTriangle
-
-    "points" is a pair of vectors:
-    - the first vector represents the x components of the vertices
-    - the second vector represents the y componnents of the vertices    
-
-*/
 TEST( GeometryTest, ConstructPositivelyOrientedTriangle ) {
-    /*
-        ( p, q, r ) is a right-angle triangle
-        p: x = 0.0, y = 0.0
-        q: x = 1.0, y = 0.0
-        q: x = 0.0, y = 1.0
-    */
+    // ( p, q, r ) is a right-angle triangle
     std::pair<double,double> p( 0.0, 0.0 );
     std::pair<double,double> q( 1.0, 0.0 );
     std::pair<double,double> r( 0.0, 1.0 );
-
     std::pair<std::vector<double>,std::vector<double>> points( 
-        { p.first, r.first, q.first }, { p.second, r.second, q.second }
+        { p.first, r.first, q.first },      // x components
+        { p.second, r.second, q.second }    // y components
     );
+    auto tri = geometry::construct_positively_oriented_triangle<double>( 
+        points, 
+        0, /* index of p in "points" */
+        1, /* index of q in "points" */
+        2  /* index of r in "points" */ );
 
     /*
-        0, 1, 2 are the indices of p, q, r in "points"
-    */
-    auto tri = geometry::construct_positively_oriented_triangle<double>( points, 0, 1, 2 );
-
-    /*
-        v, u, w are the indices of the positively-oriented triangle, which should be:
-
+        v, u, w are the order of traversal with respect to the indices 
+        of p, q, r in "points", which should be:
             0, 1, 2
-
         because p, q, r are already positively-oriented
     */
+    ASSERT_TRUE( tri );
     auto [ v, u, w ] = tri.value();
     std::tie( p, q, r ) = fs::fvm::get_triangle_points( points, v, u, w );
     auto expected_positive = fs::fvm::get_orient( p, q, r );
