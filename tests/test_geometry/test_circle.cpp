266c1f2e989d08bdec06f26b94104f13672db3f3
Nick <nicholashanson27@outlook.com>
2025-09-20 04:17:09 +0000

    R:efactor test for point-in-circle checks: Use EXPECT_EQ for individual cases
    - Replaced ASSERT_EQ with EXPECT_EQ for testing point-in-circle results.
    - Refactored tests for clarity, isolating each test case in its own
      block.

--- tests/test_geometry/test_circle.cpp
+++ tests/test_geometry/test_circle.cpp
@@ -10,25 +10,22 @@
     test if three points ( 5.0, 5.0 ), ( 0.0, 0.0 ), ( 0.0, -1.0 ) are inside, outside or on the unit circle
 */
 TEST( GeometryTests, InCircle ) {
-
     std::pair<double,double> p( -1.0, 0.0 );
     std::pair<double,double> q(  1.0, 0.0 );
     std::pair<double,double> r(  0.0, 1.0 );
-    std::pair<double,double> a(  5.0, 5.0 );
-
-    auto expected_outside = geometry::point_in_circle( p, q, r, a );
-
-    ASSERT_EQ( expected_outside, geometry::in_circle::OUTSIDE );
-
-    a = { 0.0, 0.0 };
-
-    auto expected_inside = geometry::point_in_circle( p, q, r, a );
-
-    ASSERT_EQ( expected_inside, geometry::in_circle::INSIDE );
-
-    a = { 0.0, -1.0 };
-
-    auto expected_on = geometry::point_in_circle( p, q, r, a );
-
-    ASSERT_EQ( expected_on, geometry::in_circle::ON );
+    {
+        std::pair<double,double> a( 5.0, 5.0 );
+        auto expected_outside = geometry::point_in_circle( p, q, r, a );
+        EXPECT_EQ( expected_outside, geometry::in_circle::OUTSIDE );
+    }
+    {
+        std::pair<double,double> a( 0.0, 0.0 );
+        auto expected_inside = geometry::point_in_circle( p, q, r, a );
+        EXPECT_EQ( expected_inside, geometry::in_circle::INSIDE );
+    }
+    {
+        std::pair<double,double> a( 0.0, -1.0 );
+        auto expected_on = geometry::point_in_circle( p, q, r, a );
+        EXPECT_EQ( expected_on, geometry::in_circle::ON );
+    }
 }
