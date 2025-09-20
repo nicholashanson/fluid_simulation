6f0268b56d6698974f1a6888cc5472ce79a8f20c
Nick <nicholashanson27@outlook.com>
2025-09-20 04:42:20 +0000

    Moved test for LU solve to separate file.

---
+++ tests/test_lin_alg/test_LU_solve.cpp
@@ -0,0 +1,38 @@
+#include <gtest/gtest.h>
+
+#include <lin_alg/lin_alg.hpp>
+
+TEST( LinAlgTests, LUSolve ) {
+    const size_t rows = 3;
+    const size_t columns = 3;
+    /*
+        A = [ 1.0, 1.0, 1.0       x +  y + z = 6.0
+              2.0, 3.0, 1.0      2x + 3y + z = 11.0
+              3.0, 2.0, 1.0 ]    3x + 2y + z = 10.0
+
+        solution:
+            x = 1.0
+            y = 2.0
+            z = 3.0
+    */
+    const std::array<double,rows*columns> coefficients = {
+        1.0, 1.0, 1.0, 
+        2.0, 3.0, 1.0, 
+        3.0, 2.0, 1.0,
+    };
+    const std::array<double,rows> b = {
+        6.0,    //  x +  y + z = 6.0
+        11.0,   // 2x + 3y + z = 11.0
+        10.0,   // 3x + 2y + z = 10.0 
+    };
+
+    lin_alg::matrix_<double,rows,columns> A( coefficients );
+    auto LU_decomp = lin_alg::LU_decomposition<double,rows,columns>( A );
+    ASSERT_TRUE( LU_decomp );
+    auto [ L, U, ps ] = LU_decomp.value();
+    auto solution = lin_alg::LU_solve<double,rows>( L, U, b, ps );
+    std::array<double,rows> expected_solution{ 1.0, 2.0, 3.0 };
+    for ( size_t i = 0; i < rows; ++i ) {
+        EXPECT_NEAR( solution[ i ], expected_solution[ i ], 1e-9 ); 
+    }
+}
