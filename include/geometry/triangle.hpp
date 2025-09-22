e7ef1a2a414ece11fd42721d091daa017ba5660d
Nick <nicholashanson27@outlook.com>
2025-09-22 17:38:07 +0000

    Continued to migrate triangle specific functionality to this module.

--- include/geometry/triangle.hpp
+++ include/geometry/triangle.hpp
@@ -1,10 +1,113 @@
 #ifndef TRIANGLE_HPP
 #define TRIANGLE_HPP
 
-#include <fs/fvm/triangulation.hpp>
 #include <geometry/geometry.hpp>
 
 namespace geometry {
+
+    using triangle = std::tuple<int,int,int>;
+
+    // =====================
+    //  Get Triangle Points
+    // =====================
+
+    template<typename T>
+    std::array<std::pair<T,T>,3> get_triangle_points( const std::pair<std::vector<T>,std::vector<T>>& points, 
+                                                      const size_t i, const size_t j, const size_t k ) {
+        return { std::pair<T,T>( points.first[ i ], points.second[ i ] ) , 
+                 std::pair<T,T>( points.first[ j ], points.second[ j ] ) ,
+                 std::pair<T,T>( points.first[ k ], points.second[ k ] ) };
+    }
+
+    // ============================================
+    //  Get Squared Triangle Lengths Smalled Index
+    // ============================================
+
+    template<typename T>
+    std::tuple<T,T,T,size_t> get_squared_triangle_lengths_with_smallest_index( const std::pair<T,T>& p,
+                                                                               const std::pair<T,T>& q,
+                                                                               const std::pair<T,T>& r ) {
+        const T l_1 = dist_sqr( p, q );
+        const T l_2 = dist_sqr( q, r );
+        const T l_3 = dist_sqr( r, p );
+        auto [ l_min, l_med, l_max ] = min_med_max( l_1, l_2, l_3 );
+        size_t shortest_index{};
+        if ( l_min == l_2 ) {
+            shortest_index = 1;
+        }
+        if ( l_min == l_3 ) {
+            shortest_index = 2;
+        }
+        return std::tuple( l_min, l_med, l_max, shortest_index );
+    }
+
+    // ==========================
+    //  Squared Triangle Lengths
+    // ==========================
+
+    template<typename T>
+    std::tuple<T,T,T> get_squared_triangle_lengths( const std::pair<T,T>& p, const std::pair<T,T>& q, 
+                                                    const std::pair<T,T>& r ) {
+        T l_1{};
+        T l_2{};
+        T l_3{};
+        std::tie( l_1, l_2, l_3, std::ignore ) = get_squared_triangle_lengths_with_smallest_index( p, q, r );
+        return std::make_tuple( l_1, l_2, l_3 );
+    }
+
+    // ======================
+    //  Get Triangle Lengths
+    // ======================
+
+    template<typename T>
+    std::tuple<T,T,T> get_triangle_lengths( const std::pair<T,T>& p, const std::pair<T,T>& q, const std::pair<T,T>& r ) {
+        T l_1{};
+        T l_2{};
+        T l_3{};
+        std::tie( l_1, l_2, l_3, std::ignore ) = get_squared_triangle_lengths_with_smallest_index( p, q, r );
+        return std::make_tuple( std::sqrt( l_1 ), std::sqrt( l_2 ), std::sqrt( l_3 ) );
+    }
+
+    // ==================
+    //  Get Triangle Area
+    // ===================
+    // l_1, l_2, l_3: the squared lengths of the sides of a the triangle
+    template<typename T>
+    T get_triangle_area( const T l_1, const T l_2, const T l_3 ) {
+        // area squared
+        T A_2 = ( 4 * l_1 * l_2 - ( l_1 + l_2 - l_3) * ( l_1 + l_2 - l_3 ) ) / 16;
+        if ( A_2 < 0 ) {
+            T a = std::sqrt( l_1 );
+            T b = std::sqrt( l_2 );
+            T c = std::sqrt( l_3 );
+            A_2 = ( a + ( b + c ) ) * ( c - ( a - b) ) * ( c + ( a - b ) ) * ( a + ( b - c ) ) / 16;
+        }
+        if ( A_2 < 0 ) {
+            return 0;
+        } else {
+            return std::sqrt( A_2 );
+        }
+    }
+
+    // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    template<typename T>
+    T get_triangle_area( const std::pair<T,T>& p, const std::pair<T,T>& q, const std::pair<T,T>& r ) {
+        const T A = adaptive_arithmetic::orient_2( p, q, r ) / ( T )2;
+        return A;
+    }
+
+    // ========================
+    //  Get Triangle Perimeter
+    // ========================
+
+    template<typename T>
+    T get_triangle_perimeter( const std::pair<T,T>& p, const std::pair<T,T>& q, const std::pair<T,T>& r ) {
+        T l_1{};
+        T l_2{};
+        T l_3{};
+        std::tie( l_1, l_2, l_3 ) = get_triangle_lengths( p, q, r );
+        return l_1 + l_2 + l_3;
+    }
   
     // =======================
     //  Get Triangle Centroid
@@ -15,11 +118,11 @@ namespace geometry {
     std::pair<T,T> get_triangle_centroid( const std::pair<T,T>& p, 
                                           const std::pair<T,T>& q,
                                           const std::pair<T,T>& r ) {
-        auto [ qx_prime, qy_prime ] = fs::fvm::get_difference( q, p );
-        auto [ rx_prime, ry_prime ] = fs::fvm::get_difference( r, p );
-        auto cx_prime = 2 * fs::fvm::get_mid_point( qx_prime, rx_prime ) / 3;
-        auto cy_prime = 2 * fs::fvm::get_mid_point( qy_prime, ry_prime ) / 3;
-        auto [ cx, cy ] = fs::fvm::get_sum( { cx_prime, cy_prime }, p );
+        auto [ qx_prime, qy_prime ] = get_difference( q, p );
+        auto [ rx_prime, ry_prime ] = get_difference( r, p );
+        auto cx_prime = 2 * get_mid_point( qx_prime, rx_prime ) / 3;
+        auto cy_prime = 2 * get_mid_point( qy_prime, ry_prime ) / 3;
+        auto [ cx, cy ] = get_sum( { cx_prime, cy_prime }, p );
         return { cx, cy };
     }
 
@@ -27,21 +130,13 @@ namespace geometry {
     //  Get Triangle Ortho Center
     // ===========================
 
-    template<typename I,typename T>
-    std::pair<T,T> get_triangle_ortho_center( const fs::fvm::triangulation<I,T>& tri, const fs::fvm::triangle& t ) {
-        const std::pair<T,T> p = tri.get_point( std::get<0>( t ) );
-        const std::pair<T,T> q = tri.get_point( std::get<1>( t ) );
-        const std::pair<T,T> r = tri.get_point( std::get<2>( t ) );
-
-        const T a = tri.get_weight( std::get<0>( t ) );
-        const T b = tri.get_weight( std::get<1>( t ) );
-        const T c = tri.get_weight( std::get<2>( t ) );
-
-        const T A = fs::fvm::triangle_area( p, q, r );
-
-        const T d_11 = fs::fvm::dist_sqr( p, r ) + c - a;
+    template<typename T>
+    std::pair<T,T> get_triangle_ortho_center( const std::pair<T,T>& p, const std::pair<T,T>& q, const std::pair<T,T>& r,
+                                              T a = 0, T b = 0, T c = 0 ) {
+        const T A = get_triangle_area( p, q, r );
+        const T d_11 = dist_sqr( p, r ) + c - a;
         const T d_12 = p.second - r.second;
-        const T d_21 = fs::fvm::dist_sqr( q, r ) + c - b;
+        const T d_21 = dist_sqr( q, r ) + c - b;
         const T d_22 = q.second - r.second;
         
         const T o_x = r.first + ( d_11 * d_22 - d_12 * d_21 ) / ( 4 * A );
@@ -50,7 +145,6 @@ namespace geometry {
         const T e_21 = q.first - r.first;
         const T e_22 = d_21;
         const T o_y = r.second + ( e_11 * e_22 - e_12 * e_21 ) / ( 4 * A );
-
         return { o_x, o_y };
     }
 
@@ -59,21 +153,21 @@ namespace geometry {
     // ========================================
 
     template<typename T,typename Points>
-    std::optional<fs::fvm::triangle> construct_positively_oriented_triangle( const Points& points,
-                                                                             const size_t i, 
-                                                                             const size_t j, 
-                                                                             const size_t k ) {
-        auto [ p, q, r ] = fs::fvm::get_triangle_points( points, i, j, k );
-        auto orientation = fs::fvm::get_orient( p, q, r );
-        if ( orientation == fs::fvm::orient::DEGENERATE ) {
+    std::optional<triangle> construct_positively_oriented_triangle( const Points& points,
+                                                                    const std::size_t i, 
+                                                                    const std::size_t j, 
+                                                                    const std::size_t k ) {
+        auto [ p, q, r ] = get_triangle_points( points, i, j, k );
+        auto orientation = get_orient( p, q, r );
+        if ( orientation == orient::degenerate ) {
             return std::nullopt;
         }
-        if ( orientation == fs::fvm::orient::POSITIVE ) {
-            return fs::fvm::triangle( i, j, k );
+        if ( orientation == orient::positive ) {
+            return triangle( i, j, k );
         } else {
             // switching first and second vertices switches the orientation of the 
             // triangle, in this case from negative to positive
-            return fs::fvm::triangle( j, i, k );
+            return triangle( j, i, k );
         }
     }
 
@@ -91,8 +185,8 @@ namespace geometry {
     T get_triangle_circumradius( const std::pair<T,T>& p, 
                                  const std::pair<T,T>& q, 
                                  const std::pair<T,T>& r ) {
-        const auto [ l_1_sqr, l_2_sqr, l_3_sqr ] = fs::fvm::squared_triangle_lengths( p, q, r );
-        T A = fs::fvm::triangle_area( p, q, r );
+        const auto [ l_1_sqr, l_2_sqr, l_3_sqr ] = get_squared_triangle_lengths( p, q, r );
+        T A = get_triangle_area( p, q, r );
         return get_triangle_circumradius( A, l_1_sqr, l_2_sqr, l_3_sqr );
     }
 
@@ -105,11 +199,11 @@ namespace geometry {
                                         const std::pair<T,T>& q,
                                         const std::pair<T,T>& r,
                                         const T a, const T b, const T c ) {
-        T A = fs::fvm::triangle_area( p, q, r );
-        T d_11 = fs::fvm::dist_sqr( p, r ) + c - a;
-        T d_21 = fs::fvm::dist_sqr( q, r ) + c - b;
-        auto [ e_11, d_12 ] = fs::fvm::get_difference( p, r );
-        auto [ e_21, d_22 ] = fs::fvm::get_difference( q, r );
+        T A = get_triangle_area( p, q, r );
+        T d_11 = dist_sqr( p, r ) + c - a;
+        T d_21 = dist_sqr( q, r ) + c - b;
+        auto [ e_11, d_12 ] = get_difference( p, r );
+        auto [ e_21, d_22 ] = get_difference( q, r );
 
         T e_12 = d_11;
         T e_22 = d_21;
@@ -117,20 +211,6 @@ namespace geometry {
         T t_1 = d_11 * d_22 - d_12 * d_21;
         T t_2 = e_11 * e_22 - e_12 * e_21;
         return ( t_1 * t_1 + t_2 * t_2 ) / ( 16 * A * A ) - c;
-    }
-
-    // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-    template<typename I,typename T>
-    T get_triangle_orthoradius_squared( const fs::fvm::triangulation<I,T>& tri, 
-                                        const fs::fvm::triangle& t ) {
-        const std::pair<T,T> p = tri.get_point( std::get<0>( t ) );
-        const std::pair<T,T> q = tri.get_point( std::get<1>( t ) );
-        const std::pair<T,T> r = tri.get_point( std::get<2>( t ) );
-
-        const T a = tri.get_weight( std::get<0>( t ) );
-        const T b = tri.get_weight( std::get<1>( t ) );
-        const T c = tri.get_weight( std::get<2>( t ) );
-        return get_triangle_orthoradius_squared( p, q, r, a, b, c );
     }
 
     // =======================
@@ -144,11 +224,9 @@ namespace geometry {
 
     // ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     template<typename T>
-    T get_triangle_inradius( const std::pair<T,T>& p, 
-                         const std::pair<T,T>& q, 
-                         const std::pair<T,T>& r ) {
-        T A = fs::fvm::triangle_area( p, q, r );
-        T perimeter = fs::fvm::triangle_perimeter( p, q, r );
+    T get_triangle_inradius( const std::pair<T,T>& p, const std::pair<T,T>& q, const std::pair<T,T>& r ) {
+        T A = get_triangle_area( p, q, r );
+        T perimeter = get_triangle_perimeter( p, q, r );
         return get_triangle_inradius( A, perimeter );
     }
 
@@ -161,9 +239,9 @@ namespace geometry {
                                               const std::pair<T,T>& q, 
                                               const std::pair<T,T>& r,
                                               const T A ) {
-        T d_11 = fs::fvm::dist_sqr( p, r );
+        T d_11 = dist_sqr( p, r );
         T d_12 = p.second - r.second;
-        T d_21 = fs::fvm::dist_sqr( q, r );
+        T d_21 = dist_sqr( q, r );
         T d_22 = q.second - r.second;
         T o_x = r.first + ( d_11 * d_22 - d_12 * d_21 ) / ( 4 * A );
         T e_11 = p.first - r.first;
@@ -179,7 +257,7 @@ namespace geometry {
     std::pair<T,T> get_triangle_circumcenter( const std::pair<T,T>& p, 
                                               const std::pair<T,T>& q, 
                                               const std::pair<T,T>& r ) {
-        T A = fs::fvm::triangle_area( p, q, r );
+        T A = get_triangle_area( p, q, r );
         return get_triangle_circumcenter( p, q, r, A );
     }
 
@@ -191,11 +269,11 @@ namespace geometry {
     std::tuple<T,T,T> get_triangle_angles( const std::pair<T,T>& p,
                                            const std::pair<T,T>& q,
                                            const std::pair<T,T>& r ) {
-        const T A = fs::fvm::triangle_area( p, q, r );
+        const T A = get_triangle_area( p, q, r );
         T theta_1 = calculate_theta( p, q, r, p, A );
         T theta_2 = calculate_theta( q, p, r, q, A ); 
         T theta_3 = calculate_theta( r, p, q, r, A );
-        return fs::fvm::min_med_max( theta_1, theta_2, theta_3 );
+        return min_med_max( theta_1, theta_2, theta_3 );
     }
 
 } // namespace geometry
